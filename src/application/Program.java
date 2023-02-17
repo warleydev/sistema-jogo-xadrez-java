9cb14ae69115cd0bde2e49acb49a986362017670
Warley Dev <warleycm7@gmail.com>
2023-02-17 14:30:00 -0300

    Começando a implementação do Tabuleiro e Peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
+        Board board = new Board(8, 8);  
     }
 }
