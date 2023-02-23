c1a43e918c4bb2a5d70bf5375bcecb1840101610
Warley Dev <warleycm7@gmail.com>
2023-02-22 21:26:33 -0300

    Adicionando a peça BISPO e consertando BUG na peça PEÃO

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,6 +4,7 @@ import application.UI;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -190,6 +191,8 @@ public class ChessMatch {
 
     private void initialSetup(){
         placeNewPiece('a', 1, new Rook(board, Color.BRANCO));
+        placeNewPiece('c', 1, new Bishop(board, Color.BRANCO));
+        placeNewPiece('f', 1, new Bishop(board, Color.BRANCO));
         placeNewPiece('e', 1, new King(board, Color.BRANCO));
         placeNewPiece('h', 1, new Rook(board, Color.BRANCO));
         placeNewPiece('a', 2, new Pawn(board, Color.BRANCO));
@@ -202,7 +205,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.BRANCO));
 
         placeNewPiece('a', 8, new Rook(board, Color.PRETO));
+        placeNewPiece('c', 8, new Bishop(board, Color.PRETO));
         placeNewPiece('e', 8, new King(board, Color.PRETO));
+        placeNewPiece('f', 8, new Bishop(board, Color.PRETO));
         placeNewPiece('h', 8, new Rook(board, Color.PRETO));
         placeNewPiece('a', 7, new Pawn(board, Color.PRETO));
         placeNewPiece('b', 7, new Pawn(board, Color.PRETO));
